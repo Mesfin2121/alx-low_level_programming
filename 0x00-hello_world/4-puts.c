85297362b3c2013617e4fb4a4517f5387f011436
Mesfin2121 <medmesfin20@gmail.com>
2022-09-08 15:00:21 -0700

    C program that prints exactly

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - print string
+ *
+ *
+ */
+
+int main()
+{
+    
+	char Mylang[] = "Programming is like building a multilingual puzzle";
+
+	puts(Mylang);
+
+	return 0;
+}
