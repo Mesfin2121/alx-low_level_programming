96c44d3e2b968bd77e30e0ff4977ba7bdcfcf396
Mesfin2121 <medmesfin20@gmail.com>
2022-09-09 22:48:09 -0700

    prints all the numbers of base 16 in lowercase, followed by a new line

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,20 +6,15 @@
 int main(void)
 {
 	int number, letter;
-	
+
 	for (number = 48; number < 58; number++)
 	{
 	putchar(number);
 	}
-
 	for (letter = 97; letter < 103; letter++)
 	{
 	putchar(letter);
 	}
-
 	putchar(10);
-
 	return (0);
 }
-	
-
