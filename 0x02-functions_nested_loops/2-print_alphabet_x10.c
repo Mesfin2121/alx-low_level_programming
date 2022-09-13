fa532c46185592f779230c85409cecf42a9ae4a4
Mesfin2121 <medmesfin20@gmail.com>
2022-09-13 14:17:10 -0700

    prints 10 times the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,6 @@
 #include "holberton.h"
 /**
- * print_alphabet_x10 - print alphabet 10 
- *
+ * print_alphabet_x10 - print alphabet 10
  * Return: void
  */
 void print_alphabet_x10(void)
@@ -9,14 +8,12 @@ void print_alphabet_x10(void)
 	int count;
 	char letter;
 
-	for(count = 0; count < 10; count++)
+	for (count = 0; count < 10; count++)
 	{
-		letter = 'a';
-		
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
