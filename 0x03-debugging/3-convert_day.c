8f8b80cfdbfc9f7b1dc2178a098a242fe3fdcb96
Mesfin2121 <medmesfin20@gmail.com>
2022-09-16 12:19:50 -0700

    converts a date to the day of year and determines how many days are left in the year

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * convert_day - converts day of month to day of year, without accounting
  * for leap year
@@ -7,12 +6,11 @@
  * @day: day of month
  * Return: day of year
  */
-
 int convert_day(int month, int day, int year)
 {
 	int days_in_feb = 28;
 
-	if ( (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) )
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
 	{
 		days_in_feb = 29;
 	}
@@ -26,31 +24,31 @@ int convert_day(int month, int day, int year)
 			day = 31 + days_in_feb;
 			break;
 		case 4:
-			day = 31 + days_in_feb + 31;
+			day = 62 + days_in_feb;
 			break;
 		case 5:
-			day = 31 + days_in_feb + 31 + 30;
+			day = 92 + days_in_feb;
 			break;
 		case 6:
-			day = 31 + days_in_feb + 31 + 30 + 31;
+			day = 123 + days_in_feb;
 			break;
 		case 7:
-			day = 31 + days_in_feb + 31 + 30 + 31 + 30;
+			day = 153 + days_in_feb;
 			break;
 		case 8:
-			day = 31 + days_in_feb + 31 + 30 + 31 + 30 + 31;
+			day = 184 + days_in_feb;
 			break;
 		case 9:
-			day = 31 + days_in_feb + 31 + 30 + 31 + 30 + 31 + 31;
+			day = 215 + days_in_feb;
 			break;
 		case 10:
-			day = 31 + days_in_feb + 31 + 30 + 31 + 30 + 31 + 31 + 30;
+			day = 246 + days_in_feb;
 			break;
 		case 11:
-			day = 31 + days_in_feb + 31 + 30 + 31 + 30 + 31 + 30 + 31 + 31;
+			day = 276 + days_in_feb;
 			break;
 		case 12:
-			day = 31 + days_in_feb + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30;
+			day = 306 + days_in_feb;
 			break;
 	}
 	return (day);
