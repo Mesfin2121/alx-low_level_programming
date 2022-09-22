9d38869e1c4cb85eebfde35cb1bcf1ea40a372d3
Mesfin2121 <medmesfin20@gmail.com>
2022-09-21 20:02:33 -0700

    concatenates two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncat - concationation
+ * @dest: destination
+ * @src: source
+ * @n:nubert of bytes
+ * Return: char
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int len = 0;
+
+	while (dest[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[len + i] = src[i];
+	dest[len + i] = '\0';
+
+	return (dest);
+}
