a1028f39e48cb92e714f4a2dca095f9566b728ef
Mesfin2121 <medmesfin20@gmail.com>
2022-12-17 12:25:30 -0800

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -5,7 +5,7 @@ char *_strcpy(char *dest, char *src);
  * new_dog - creates a new dog structure.
  * @name: dog's name.
  * @age: dog's age.
- * owner: dog's owner.
+ * @owner: dog's owner.
  * Return: the new dog structure.
  */
 dog_t *new_dog(char *name, float age, char *owner)
