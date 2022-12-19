2e369c9c548394f182f7f396771f8753d437cdcf
Mesfin2121 <medmesfin20@gmail.com>
2022-12-19 13:31:17 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
-#include<stdio.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
- * main - entry point
- * Return: 1 error
+ * main - Entry point to prints exactly that useful piece of art is useful,
+ * - Dora Korpar, 2015-10-19",
+ *   followed by a new line, to the standard error.]
+ *   Return: Always 0 (Success), else none zero.
  */
 int main(void)
 {
-write(2, "and that piece of art is usefull\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
