85add2e57d3e314809252257fcb371d7467b24f1
Mesfin2121 <medmesfin20@gmail.com>
2022-12-19 11:41:40 -0800

    Task 3

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -6,49 +6,44 @@
  * @day: day of month
  * Return: day of year
  */
-int convert_day(int month, int day, int year)
+int convert_day(int month, int day)
 {
-	int days_in_feb = 28;
-
-	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
-	{
-		days_in_feb = 29;
-	}
-
 	switch (month)
 	{
 		case 2:
 			day = 31 + day;
 			break;
 		case 3:
-			day = 31 + days_in_feb;
+			day = 59 + day;
 			break;
 		case 4:
-			day = 62 + days_in_feb;
+			day = 90 + day;
 			break;
 		case 5:
-			day = 92 + days_in_feb;
+			day = 120 + day;
 			break;
 		case 6:
-			day = 123 + days_in_feb;
+			day = 151 + day;
 			break;
 		case 7:
-			day = 153 + days_in_feb;
+			day = 181 + day;
 			break;
 		case 8:
-			day = 184 + days_in_feb;
+			day = 212 + day;
 			break;
 		case 9:
-			day = 215 + days_in_feb;
+			day = 243 + day;
 			break;
 		case 10:
-			day = 246 + days_in_feb;
+			day = 273 + day;
 			break;
 		case 11:
-			day = 276 + days_in_feb;
+			day = 304 + day;
 			break;
 		case 12:
-			day = 306 + days_in_feb;
+			day = 334 + day;
+			break;
+		default:
 			break;
 	}
 	return (day);
