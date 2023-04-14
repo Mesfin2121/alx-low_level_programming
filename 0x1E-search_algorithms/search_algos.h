81a6ef9f3b2684a5e6efbcf9898d5128cdb8ad39
Mesfin2121 <medmesfin20@gmail.com>
2023-04-14 06:58:48 -0700

    Task 0

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef SEARCH
+#define SEARCH
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+
+/* mandatory tasks*/
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
