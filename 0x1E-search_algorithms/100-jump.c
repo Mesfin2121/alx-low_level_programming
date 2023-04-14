477999c52193f2416e766d307b5ff222ed4400ef
Mesfin2121 <medmesfin20@gmail.com>
2023-04-14 07:27:06 -0700

    Task 7

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,17 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - searches for a value in a sorted array of integers,
+ * using the Jump search algorithm.
+ * @array: pointer to the first element of the array to search in.
+ * @size: number of elements in array.
+ * @value: value to search for.
+ * Return: first index where value is located,
+ * or -1 if value is not present or array is NULL.
+ **/
+int jump_search(int *array, size_t size, int value)
+{
+	if (!array)
+		return (-1);
+	return (-1);
+}
